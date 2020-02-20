5bc3859728637bf05b96c6b8c6f4a51db22d422b
mmatthew <takeknock@gmail.com>
2020-02-20 23:37:00 +0900

    feat: add initial controller

---
+++ SampleMVC/Controllers/CashflowGeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public class CashflowGeneratorController : Controller
+    {
+        // GET: CashflowGenerator
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "This is my <b>default</b> aciton ";
+        }
+
+        public string Sample(string businessCenters)
+        {
+            return $"This fixed cashflow is made from {businessCenters}";
+        }
+    }
+}
